6e37082b50b3ce8589a530169d3ae8ca1c686a1a
Johnny Gonzalez <jgonzalez@JGONZALEZ-XPS.corp.natpal.com>
2015-09-22 17:46:54 -0400

    Get-LastLogonTime

---
+++ get-lastlogontime.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-LastLogonTime 
+{
+    $user = Read-Host "Enter username of AD user to check"
+    $output = @()
+    $DCs = Get-QADComputer -searchroot 'corp.natpal.com/Domain Controllers'
+    foreach($DC in $DCs)
+    {
+      Connect-QADService $DC.Name
+      $result = Get-QADUser -Identity NATPAL\$user | Select LastLogon
+      $customObj = [PSCustomObject] @{DC=$DC;LastLogon = $result}
+      $output += $customObj
+    }
+    $output | Sort LastLogon | FT -AutoSize
+}
+
+Get-LastLogonTime
