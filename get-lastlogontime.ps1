f59a2440eaef2227ef2fb7014081267a65b339ea
Johnny Gonzalez <jgonzalez@JGONZALEZ-XPS.corp.natpal.com>
2015-09-22 18:01:02 -0400

    Domain change

---
+++ get-lastlogontime.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-LastLogonTime 
+{
+    $user = Read-Host "Enter username of AD user to check"
+    $output = @()
+    $DCs = Get-QADComputer -searchroot 'domain.com/Domain Controllers'
+    foreach($DC in $DCs)
+    {
+      Connect-QADService $DC.Name
+      $result = Get-QADUser -Identity DOMAIN\$user | Select LastLogon
+      $customObj = [PSCustomObject] @{DC=$DC;LastLogon = $result}
+      $output += $customObj
+    }
+    $output | Sort LastLogon | FT -AutoSize
+}
+
+Get-LastLogonTime
